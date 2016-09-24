3180a9d13681223f0d63998cc158adedfc49a970
Kamil Markiewicz <k.a.markiewicz@gmail.com>
2016-09-24 20:35:55 +0200

    lets use express.js...

--- server.js
+++ server.js
@@ -1,40 +1,13 @@
-var http = require('http');
-var dispatcher = require('httpdispatcher');
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
 
+app.use(bodyParser.urlencoded({extended : true}));
 
-//For all your static (js/css/images/etc.) set the directory name (relative path).
-dispatcher.setStatic('resources');
-
-//A sample GET request
-dispatcher.onGet("/page1", function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Page One');
+app.post("/pathpostdataissentto", function(request, response) {
+  console.log(request.body);
+  //Or
+  console.log(request.body.fieldName);
 });
 
-//A sample POST request
-dispatcher.onPost("/post1", function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Got Post Data');
-});
-
-//Lets use our dispatcher
-function handleRequest(request, response){
-    try {
-        //log the request on console
-        console.log(request.url);
-        //Disptach
-        dispatcher.dispatch(request, response);
-    } catch(err) {
-        console.log(err);
-    }
-}
-
-//Create a server
-var server = http.createServer(handleRequest);
-
-//Lets start our server
-const PORT=8080;
-server.listen(8080, function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", 8080);
-});
+app.listen(8080);
