284ee84fb71524ff629479568e5cb7cb34f4b86f
Kamil Markiewicz <k.a.markiewicz@gmail.com>
2016-09-25 07:30:13 +0200

    ...

--- poprawne_dane/symbioza.py
+++ poprawne_dane/symbioza.py
@@ -26,24 +26,26 @@ def wspolrzedne_key(row):
 
 subres = []
 import editdistance
-for x in lacznik:
-    x_teryt, x_simc, x_name = x['teryt'], x['simc'], x['name'].lower()
-    def join1():
-        for p in ludnosc:
-            p_teryt, p_simc = ludnosc_key(p):
-            if x_teryt == p_teryt[:4] and x_simc == p_simc:
-                # SIMC jest pewnikiem
-                r = p.clone()
-                r.update(x)
-                yield r
-    res += join1()
 
-for x in res:
-    def join2():
-        for q in wspolrzedne:
-            q_teryt, q_name = wspolrzedne_key(q)
-            if x_teryt == q_teryt:
-                # TERYT jest pewnikiem
-                for p in _ludnosc:
-                    if editdistance.eval(x_name, q_name.lower()) <= 3 or editdistance.eval(p_name.lower(), q_name.lower()) <= 3:
-                        p | q
+def join1():
+    for x in lacznik:
+        x_teryt, x_simc = x['teryt'], x['simc']
+            for p in ludnosc:
+                p_teryt, p_simc = ludnosc_key(p)
+                if x_teryt == p_teryt[:4] and x_simc == p_simc:
+                    # SIMC jest pewnikiem
+                    r = p.clone()
+                    r.update(x)
+                    yield r
+
+def join2():
+    for x in join1():
+        x_teryt, x_name = x['teryt'], x['name']
+            for q in wspolrzedne:
+                q_teryt, q_name = wspolrzedne_key(q)
+                if editdistance.eval(x_name, q_name.lower()) <= 3:
+                    r = q.clone()
+                    r.update(x)
+                    yield r
+
+open('BAZA.json', 'w', encoding='utf-8').write(json.dumps(list(join2())))
